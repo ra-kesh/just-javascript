5656fc51517ee2040f607742d7d75e38a691bd90
Rakesh <rakesh1q2w@gmail.com>
2020-12-16 03:32:44 +0530

    initial commit version one

--- src/App.js
+++ src/App.js
@@ -1,23 +1,143 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { database } from "./videodb.js";
+import { useState } from "react";
+
+const videoDb = Object.keys(database);
 
 function App() {
+
+  const [videoList, setVideoList] = useState("Closures");
+
+  function videoCLickHandeller(videos){
+    setVideoList(videos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <div className="strip">
+
+          </div>
+        <div className="container-main">
+          <div className="container-hero">
+           
+           <div className="text-hero">
+                <h1>Just JavaScript</h1>
+                <div>
+                <p>Deepdive into the world of javascript with this free curated content.</p>
+                </div>
+               
+           </div>
+          
+
+               
+          </div>
+          <div className="container-category">
+           {
+              videoDb.map((videos,index)=>{
+                return <button
+                key={index}
+                onClick={()=>videoCLickHandeller(videos)}
+                > {videos}
+                </button>; 
+              })
+            } 
+          </div>
+          <div className="container-list">
+
+            {
+              database[videoList].map(video=>(
+
+                  <div>
+                         <div className="div-video">
+                      <div className="video">
+                            <iframe 
+                            title={video.title}
+                            src={video.url} 
+                            width="400" height="200"
+                            frameBorder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+
+                      </div>
+                      <div className="description">
+                          <div>
+                           <h2>{video.title}</h2>
+                           </div>
+                    
+                      </div>
+              </div>
+
+                  </div>
+           
+              ))
+            }
+              
+          </div>
+          
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      {/* <h1>just javascript</h1>
+      
+        {
+
+            videoDb.map((videos,index)=>{
+              return <button
+              key={index}
+              onClick={()=>videoCLickHandeller(videos)}
+              > {videos}
+              </button>; 
+            })
+        } 
+
+
+          {
+            database[videoList].map(video => (
+            <div>
+                <div>           
+                  <iframe 
+                  title={video.title}
+                  width="853" height="480" src={video.url} frameBorder="0" allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+                </div>
+                <div>
+                  <p><a href= {video.url}>full screen</a></p>
+                </div>
+           </div>
+             
+               
+              
+            ))
+          }
+          
+         */}
+
+          
+      
     </div>
   );
 }
---
+++ src/videodb.js
@@ -0,0 +1,156 @@
+export var database = {
+  "All videos" : [
+    {
+        title: "How Does Javascript Work? - Andrei Neagoie",
+        url: "https://www.youtube.com/embed/hGSHfObcVf4",
+        channelName:"Udemy Tech",
+        channelUrl:"https://www.youtube.com/channel/UCU6e4MJtvlcX5DBLP1cq8hQ"
+    },
+  
+    {
+        title: "Understand the context in javascript",
+        url: "https://www.youtube.com/embed/_JMXlGt3OSo",
+        channelName:"Hitesh Choudhary",
+        channelUrl:"https://www.youtube.com/channel/UCXgGY0wkgOzynnHvSEVmE3A"
+    },
+
+    {
+        title: "Scope chaining in javascript",
+        url: "https://www.youtube.com/embed/_JMXlGt3OSo",
+        channelName:"Hitesh Choudhary",
+        channelUrl:"https://www.youtube.com/channel/UCXgGY0wkgOzynnHvSEVmE3A"
+    },
+  
+    {
+        title: "The Scope Chain, 🔥Scope & Lexical Environment",
+        url: "https://www.youtube.com/embed/uH-tVP8MUs8",
+        channelName:"Akshay Saini",
+        channelUrl:"https://www.youtube.com/channel/UC3N9i_KvKZYP4F84FPIzgPQ"
+    },
+    {
+        title: "Learn Closures In 7 Minutes",
+        url: "https://www.youtube.com/embed/3a0I8ICR1Vg",
+        channelName:"Web Dev Simplified",
+        channelUrl:"https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw"
+    },
+
+    {
+        title: "JavaScript Promises In 10 Minutes",
+        url: "https://www.youtube.com/embed/DHvZLI7Db8E",
+        channelName:"Web Dev Simplified",
+        channelUrl:"https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw"
+    },
+    {
+        title: "JavaScript Async Await",
+        url: "https://www.youtube.com/embed/V_Kr9OSfDeU",
+        channelName:"Web Dev Simplified",
+        channelUrl:"https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw"
+    },
+    {
+        title: "JavaScript ES6 Modules",
+        url: "https://www.youtube.com/embed/cRHQNNcYf6s",
+        channelName:"Web Dev Simplified",
+        channelUrl:"https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw"
+    },
+    {
+        title: "Immediately Invoked Function Expression",
+        url: "https://www.youtube.com/embed/3cbiZV4H22c",
+        channelName:"freeCodeCamp.org",
+        channelUrl:"https://www.youtube.com/channel/UC8butISFwT-Wl7EV0hUK0BQ"
+    },
+  
+  ],
+
+  "Inner Workings": [
+    {
+      title: "How Does Javascript Work? - Andrei Neagoie",
+      url: "https://www.youtube.com/embed/hGSHfObcVf4",
+      channelName:"Udemy Tech",
+      channelUrl:"https://www.youtube.com/channel/UCU6e4MJtvlcX5DBLP1cq8hQ"
+    },
+  
+  ],
+
+  "Execution Context": [
+    {
+      title: "Understand the context in javascript",
+      url: "https://www.youtube.com/embed/_JMXlGt3OSo",
+      channelName:"Hitesh Choudhary",
+      channelUrl:"https://www.youtube.com/channel/UCXgGY0wkgOzynnHvSEVmE3A"
+    },
+  
+  ],
+
+  "Hoisting": [
+    {
+      title: "Hoisting in JavaScript 🔥(variables & functions)",
+      url: "https://www.youtube.com/embed/vGGOOYOXoQ4",
+      channelName:"Akshay Saini",
+      channelUrl:"https://www.youtube.com/channel/UC3N9i_KvKZYP4F84FPIzgPQ"
+    },
+  
+  ],
+
+  "Scopes": [
+     {
+        title: "Scope chaining in javascript",
+        url: "https://www.youtube.com/embed/_JMXlGt3OSo",
+        channelName:"Hitesh Choudhary",
+        channelUrl:"https://www.youtube.com/channel/UCXgGY0wkgOzynnHvSEVmE3A"
+      },
+  
+      {
+        title: "The Scope Chain, 🔥Scope & Lexical Environment",
+        url: "https://www.youtube.com/embed/uH-tVP8MUs8",
+        channelName:"Akshay Saini",
+        channelUrl:"https://www.youtube.com/channel/UC3N9i_KvKZYP4F84FPIzgPQ"
+      },
+  
+  ],
+
+  "Closures": [
+    {
+      title: "Learn Closures In 7 Minutes",
+      url: "https://www.youtube.com/embed/3a0I8ICR1Vg",
+      channelName:"Web Dev Simplified",
+      channelUrl:"https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw"
+    },
+  
+  ],
+
+
+  "Asynchronous JS": [
+    {
+      title: "JavaScript Promises In 10 Minutes",
+      url: "https://www.youtube.com/embed/DHvZLI7Db8E",
+      channelName:"Web Dev Simplified",
+      channelUrl:"https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw"
+    },
+    {
+      title: "JavaScript Async Await",
+      url: "https://www.youtube.com/embed/V_Kr9OSfDeU",
+      channelName:"Web Dev Simplified",
+      channelUrl:"https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw"
+    },
+  
+  ],
+  "IIFE and Modules": [
+    {
+      title: "JavaScript ES6 Modules",
+      url: "https://www.youtube.com/embed/cRHQNNcYf6s",
+      channelName:"Web Dev Simplified",
+      channelUrl:"https://www.youtube.com/channel/UCFbNIlppjAuEX4znoulh0Cw"
+    },
+    {
+      title: "Immediately Invoked Function Expression ",
+      url: "https://www.youtube.com/embed/3cbiZV4H22c",
+      channelName:"freeCodeCamp.org",
+      channelUrl:"https://www.youtube.com/channel/UC8butISFwT-Wl7EV0hUK0BQ"
+    },
+  
+  ],
+ 
+};
+
+
+// <iframe width="853" height="480" src="https://www.youtube.com/embed/3cbiZV4H22c" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
